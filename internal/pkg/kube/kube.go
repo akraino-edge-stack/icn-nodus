378bc32fc5f7df77a70b82abc627f4f5ffa7c829
Kuralamudhan Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
2021-01-27 11:10:30 -0800

    adding sfc features
    Signed-off-by: Kural Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
    Change-Id: I4dcdb87c314fcf2b90d037736501c7ba5610bfb2

--- internal/pkg/kube/kube.go
+++ internal/pkg/kube/kube.go
@@ -2,7 +2,9 @@ package kube
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/prometheus/common/log"
 	"github.com/sirupsen/logrus"
 
 	kapi "k8s.io/api/core/v1"
@@ -11,7 +13,12 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
+
+const apiserverNamespace = "kube-system"
+const apiserverPodName = "kube-apiserver-master"
+const controlplaneOption = "--service-cluster-ip-range"
 
 // Interface represents the exported methods for dealing with getting/setting
 // kubernetes resources
@@ -35,6 +42,23 @@ type Interface interface {
 // Kube is the structure object upon which the Interface is implemented
 type Kube struct {
 	KClient kubernetes.Interface
+}
+
+func GetKubeConfig() (*kubernetes.Clientset, error) {
+	var k *kubernetes.Clientset
+	// Get a config to talk to the apiserver
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	k, err = kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Error(err, "Error building Kuberenetes clientset")
+		return nil, err
+	}
+
+	return k, nil
 }
 
 // SetAnnotationOnPod takes the pod object and key/value string pair to set it as an annotation
@@ -74,6 +98,32 @@ func (k *Kube) SetAnnotationOnNamespace(ns *kapi.Namespace, key,
 			ns.Name, err)
 	}
 	return err
+}
+
+func findServiceIP(a []string, key string) string {
+	var clusterCidr string
+	for _, n := range a {
+		parts := strings.Split(n, "=")
+		if len(parts) == 1 {
+			continue
+		}
+		if strings.Compare(parts[0], key) == 0 {
+			clusterCidr = parts[1]
+			break
+		}
+	}
+	return clusterCidr
+}
+
+// GetControlPlaneServiceIPRange return the service IP
+func (k *Kube) GetControlPlaneServiceIPRange() (string, error) {
+	var clusterIP string
+	pod, err := k.GetPod(apiserverNamespace, apiserverPodName)
+	if err != nil {
+		logrus.Errorf("Error in getting the pod spec details of kube-apiserver-master: %v", err)
+	}
+	clusterIP = findServiceIP(pod.Spec.Containers[0].Command, controlplaneOption)
+	return clusterIP, nil
 }
 
 // GetAnnotationsOnPod obtains the pod annotations from kubernetes apiserver, given the name and namespace

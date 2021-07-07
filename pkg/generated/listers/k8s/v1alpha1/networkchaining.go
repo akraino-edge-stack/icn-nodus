87db8d0a7c3b3527180c285725beb7703988b157
Kuralamudhan Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
2021-07-07 16:00:07 -0700

    [1/2] changing ovn4k8s-nfv-plugin package to nodus
    Signed-off-by: Kural Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
    Change-Id: If73cfd393e1a77abc2a54b59612127a5c4c7ca3b

--- pkg/generated/listers/k8s/v1alpha1/networkchaining.go
+++ pkg/generated/listers/k8s/v1alpha1/networkchaining.go
@@ -19,7 +19,7 @@ limitations under the License.
 package v1alpha1
 
 import (
-	v1alpha1 "icn-nodus/pkg/apis/k8s/v1alpha1"
+	v1alpha1 "github.com/akraino-edge-stack/icn-nodus/pkg/apis/k8s/v1alpha1"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
